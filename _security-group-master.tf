83d84ee5fc9eb9fd2f0e8e6ec9753b23b139bcdb
stephanlindauer <stephanlindauer@posteo.de>
2016-12-21 15:18:49 +0100

    opened firewall setting for testing

--- _security-group-master.tf
+++ _security-group-master.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "k8s-master" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # api-server
   ingress {
     from_port   = 443
--- _security-group-worker.tf
+++ _security-group-worker.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "k8s-worker" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
